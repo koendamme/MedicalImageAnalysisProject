7615bafffb5d69827b93617fa84041110fd32266
DH-is-great <r.schrijver-1@student.utwente.nl>
2023-06-15 09:51:54 +0200

    metric function beginning

---
+++ eval_metrics.py
@@ -0,0 +1,41 @@
+import numpy as np
+from tqdm import tqdm
+import monai
+import torch
+from ACDCDataset import ACDCDataset
+import os
+from custom_transforms import GroundTruthTransform
+import json
+from datetime import datetime
+import wandb
+from custom_loss import CoshDiceLoss
+import math
+import matplotlib.pyplot as plt
+from PIL import Image
+
+def compute_metric(dataloader, model, metric_fn, device):
+    """
+    This function computes the average value of a metric for a data set.
+
+    Args:
+        dataloader (monai.data.DataLoader): dataloader wrapping the dataset to evaluate.
+        model (torch.nn.Module): trained model to evaluate.
+        metric_fn (function): function computing the metric value from two tensors:
+            - a batch of outputs,
+            - the corresponding batch of ground truth masks.
+
+    Returns:
+        (float) the mean value of the metric
+    """
+    model.eval()
+    inferer = monai.inferers.SlidingWindowInferer(roi_size=[200, 200])
+    discrete_transform = monai.transforms.AsDiscrete(threshold=0.5)
+    Sigmoid = torch.nn.Sigmoid()
+
+    for sample in dataloader:
+        with torch.no_grad():
+            output_class1 = discrete_transform(Sigmoid(inferer(sample['img'].to(device), network=model).cpu()))
+
+        batch_class_dice = metric_fn(y_pred=output_class1, y=sample["mask"], include_background=False)
+
+    return batch_class_dice, batch_class_dice.mean(dim=0)
